1570e8b91c70228612d7fb94dc9b95485734bb3b
Luillin Escobar <luillin.escobar@hotmail.com>
2022-05-09 01:05:56 -0500

    resolvemos ejercicio de ciclistas

--- clases/Ciclista.py
+++ clases/Ciclista.py
@@ -1,24 +1,48 @@
-from clases.Persona import Persona
-
-class Ciclista(Persona):
+class Ciclista():
     def __int__(self):
-        self.__equipo = None
+        self.__nombre = None
+        self.__edad = None
+        self.__pais = None
         self.__tiempoPrueba = None
 
     # Metodo Get (Obtener el valor de un atrubuto)
     @property
-    def equipo(self):
-        return self.__equipo
+    def nombre(self):
+        return self.__nombre
+    
+    @property
+    def edad(self):
+        return self.__edad
+
+    @property
+    def pais(self):
+        return self.__pais
 
     @property
     def tiempoPrueba(self):
         return self.__tiempoPrueba
 
+    
+
     # Metodo set (Lleva un valor a un atrubuto)
-    @equipo.setter
-    def equipo(self, equipo):
-        self.__equipo = equipo
+    @nombre.setter
+    def nombre(self, nombre):
+        self.__nombre = nombre
+
+    @edad.setter
+    def edad(self, edad):
+        self.__edad = edad
+
+    @pais.setter
+    def pais(self, pais):
+        self.__pais = pais
 
     @tiempoPrueba.setter
     def tiempoPrueba(self, tiempoPrueba):
         self.__tiempoPrueba = tiempoPrueba
+
+    def print(self):
+        print("nombre =", self.nombre)
+        print("edad =", str(self.edad))
+        print("pais =", self.pais)
+        print("tiempoPrueba =", str(self.tiempoPrueba))
--- main.py
+++ main.py
@@ -1,11 +1,44 @@
+import pprint
 from clases.Ciclista import Ciclista
 from clases.Cliente import Cliente
 
-ciclista = Ciclista()
-cliente = Cliente()
 
-ciclista.nombre = "Andrey Dquue"
-cliente.nombre = "Luillin Escobar"
+opcion = input('Seleccione una opcion:\nA) Programa de ciclistas.\nB) Programa de clientes.\nExit) Para salir.\n')
+ciclistas = [];
 
-print(ciclista.nombre)
-print(cliente.nombre)
+while opcion!= 'exit':
+    if opcion == 'A':
+        ciclista = Ciclista()
+        ciclista.nombre = input('nombre: ')
+        ciclista.edad = int(input('edad: '))
+        ciclista.pais = input('pais: ')
+        ciclista.tiempoPrueba = int(input('tiempo de prueba: '))
+        ciclistas.append(ciclista);
+        opcion = input('\nSeleccione una opcion:\nA) Programa de ciclistas.\nB) Programa de clientes.\nExit) Para salir.\n')
+
+
+
+
+
+    elif opcion=='B':
+        print('clientes')
+        opcion = input('\nSeleccione una opcion:\nA) Programa de ciclistas.\nB) Programa de clientes.\nExit) Para salir.\n')
+
+    else:
+        opcion = input('\nSeleccione una opcion:\nA) Programa de ciclistas.\nB) Programa de clientes.\nExit) Para salir.\n')
+
+ganador = Ciclista()
+ganador.tiempoPrueba = 9999999999
+
+for ciclista in ciclistas:
+    if ganador.tiempoPrueba > ciclista.tiempoPrueba:
+        ganador.nombre = ciclista.nombre
+        ganador.edad = ciclista.edad
+        ganador.pais = ciclista.pais
+        ganador.tiempoPrueba = ciclista.tiempoPrueba
+        
+
+print(ganador.nombre)
+print(ganador.edad)
+print(ganador.pais)
+print(ganador.tiempoPrueba)
